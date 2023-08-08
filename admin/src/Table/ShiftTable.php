6f6768e090317093b533bd7bb9b30f3436ed6330
Theofilos Intzoglou <int.teo@gmail.com>
2023-08-08 13:14:06 +0300

    Fix table constructors

--- admin/src/Table/ShiftTable.php
+++ admin/src/Table/ShiftTable.php
@@ -3,12 +3,13 @@
 namespace DSEWestThessaloniki\Component\Schoolsj3\Administrator\Table;
 
 use Joomla\CMS\Table\Table;
+use Joomla\Database\DatabaseDriver;
 
 defined('_JEXEC') or die;
 
 class ShiftTable extends Table
 {
-    public function __construct($table, $key, $db)
+    public function __construct(DatabaseDriver $db)
     {
 	    parent::__construct('#__sch3_shifts', 'id', $db);
     }
